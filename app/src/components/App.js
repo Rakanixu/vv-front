d94785885db6d2ff20e8f6bc1274adca2dd77993
Pablo Aguirre <paguirrerubio@gmail.com>
2017-07-18 12:18:43 +0200

    Add project template

---
+++ app/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Nav from './Nav';
+import logo from './../images/logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
