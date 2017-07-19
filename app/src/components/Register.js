51ed2d003b081cd1a5d800b1143348c8de82195c
Pablo Aguirre <paguirrerubio@gmail.com>
2017-07-19 16:49:31 +0200

    Basic validation

--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
+import ErrorReporting from 'material-ui-error-reporting';
 import TextField from 'material-ui/TextField';
 import Checkbox from 'material-ui/Checkbox';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -7,12 +8,15 @@ import axios from 'axios';
 import './Register.css';
 
 const config = require('./../config.json');
+const _ = require('lodash/core');
 
 class Register extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      error: null
+    };
   }
 
   _createPrincipal() {
@@ -41,36 +45,69 @@ class Register extends Component {
     var state = {}
     state[e.target.dataset.val] = e.target.value;
     this.setState(state);
+    this.setState({ error: null });
   }
 
   _handleRegister(e) {
-    console.log(this.state);
-    if (this.state.password && this.state.password !== this.state.passwordConfirmation) {
-      console.log("pass do not match");
+    if (_.isEmpty(this.state) || 
+      !this.refs.checkbox.state.switched ||
+      this.state.username == undefined ||
+      this.state.password == undefined ||
+      this.state.email === undefined ||
+      this.state.password !== this.state.passwordConfirmation) {
+
+      this.setState({
+        error: new Error('Invalid data')
+      });
+
+      setTimeout(function() {
+        this.setState({ error: null });
+      }.bind(this), 5000);
       return;
     }
 
-    this._createPrincipal().then(this._createUser.bind(this)).catch(err => {
+    this._createPrincipal()
+    .then(this._createUser.bind(this))
+    .then(res => {
+      // TODO: redirect or something
+    })
+    .catch(err => {
       console.log(err);
     });
   }
 
   render() {
     return (
-      <form className="registerForm">
-        <TextField floatingLabelText="Username" data-val="username" onChange={this._handleTextFieldChange.bind(this)} fullWidth={true} />
-        <TextField floatingLabelText="Email" data-val="email" onChange={this._handleTextFieldChange.bind(this)} fullWidth={true} />
-        <TextField floatingLabelText="Password" data-val="password" onChange={this._handleTextFieldChange.bind(this)} fullWidth={true} />
-        <TextField floatingLabelText="Password confirmation" data-val="passwordConfirmation" onChange={this._handleTextFieldChange.bind(this)} fullWidth={true} />
-        <div className="verticalMargin leftMargin">
-          <Checkbox label={(
-            "I accept the "  + <a to="/terms">Terms</a> + " and Data Protection Policy of vVents LLC"
-          )}/>
-        </div>
-        <div className="verticalMargin">
-          <RaisedButton label="Register" fullWidth={true} onTouchTap={this._handleRegister.bind(this)} />
-        </div>
-      </form>
+      <div>
+        <ErrorReporting open={this.state.error !== null}
+                        error={this.state.error} />
+        <form className="registerForm">
+          <TextField floatingLabelText="Username" 
+                     data-val="username"
+                     onChange={this._handleTextFieldChange.bind(this)} 
+                     fullWidth={true} />
+          <TextField floatingLabelText="Email" 
+                     data-val="email" 
+                     onChange={this._handleTextFieldChange.bind(this)} 
+                     fullWidth={true} />
+          <TextField floatingLabelText="Password" 
+                     data-val="password" 
+                     onChange={this._handleTextFieldChange.bind(this)} 
+                     fullWidth={true} />
+          <TextField floatingLabelText="Password confirmation" 
+                     data-val="passwordConfirmation" 
+                     onChange={this._handleTextFieldChange.bind(this)} 
+                     fullWidth={true} />
+          <div className="verticalMargin leftMargin">
+            <Checkbox ref="checkbox" label={(
+              "I accept the <a to='/terms'>Terms</a>  and Data Protection Policy of vVents LLC"
+            )}/>
+          </div>
+          <div className="verticalMargin">
+            <RaisedButton label="Register" fullWidth={true} onTouchTap={this._handleRegister.bind(this)} />
+          </div>
+        </form>
+      </div>
     );
   }
 }
