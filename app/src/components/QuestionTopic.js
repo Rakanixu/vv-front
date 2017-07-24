ee64d2672cbbf437b3e203363b8034c19594d491
Pablo Aguirre <paguirrerubio@gmail.com>
2017-07-24 15:26:19 +0200

    Add question topics

---
+++ app/src/components/QuestionTopic.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import ErrorReporting from 'material-ui-error-reporting';
+import axios from 'axios';
+import './QuestionTopic.css';
+
+axios.defaults.withCredentials = true; 
+
+const config = require('./../config.json');
+
+var styles = {
+  fit: {
+    overflow: 'hidden',
+    maxHeight: 400
+  }, 
+  screenHeight: {
+    height: window.innerHeight - 220
+  }
+};
+
+class QuestionTopic extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      count: 0,
+      url: config.baseAPI_URL + '/event/'
+    };
+  }
+
+  _handleTextFieldChange(e) {
+    var state = {}
+    state[e.target.dataset.val] = e.target.value;
+    this.setState(state);
+    this.setState({ error: null });
+  }
+
+  _handleNewQuestionTopic(e) {
+    this._createQuestionTopic()
+    .then(function(res) {
+      var count = this.state.count;
+      count++;
+
+      this.setState({
+        error: null,
+        count: count,
+        topic: '',
+        description: ''
+      });
+    }.bind(this))
+    .catch(err => {
+      this._handleError(err);
+    });
+  }
+
+  _createQuestionTopic() {
+    var data = new FormData();
+    data.append('topic', this.state.topic);
+    data.append('description', this.state.description);
+
+    return axios.post(this.state.url + this.props.eventId + '/question_topic', data);
+  }
+
+  _handleError(err) {
+    if (!err) {
+      err = new Error('Invalid data');
+    }
+
+    this.setState({
+      error: err
+    });
+
+    setTimeout(function() {
+      this.setState({ error: null });
+    }.bind(this), 5000);
+  }
+
+  render() {
+    return (
+      <div className="container" key={this.state.count} style={styles.screenHeight}>
+        <div className="inner-container">
+          <ErrorReporting open={this.state.error !== null}
+                    error={this.state.error} />
+
+          <form className="newQuestionTopic">
+            <TextField floatingLabelText="Question topic" 
+                      data-val="topic"
+                      onChange={this._handleTextFieldChange.bind(this)} 
+                      fullWidth={true} />                  
+            <TextField floatingLabelText="Description"
+                      data-val="description"
+                      onChange={this._handleTextFieldChange.bind(this)} 
+                      fullWidth={true} />     
+
+            <RaisedButton label="Save Question Topic" fullWidth={true} onTouchTap={this._handleNewQuestionTopic.bind(this)} />
+          </form>
+
+          <RaisedButton label="Continue" fullWidth={true} onTouchTap={this.props.onDone.bind(null, this.props.eventId)} />
+        </div>  
+      </div>
+    );
+  }
+}
+
+export default withRouter(QuestionTopic);
