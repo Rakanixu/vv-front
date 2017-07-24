434e6a73dce821cad1de0ece68098828fc3d6855
Pablo Aguirre <paguirrerubio@gmail.com>
2017-07-24 09:03:27 +0200

    Media

--- app/src/components/Media.js
+++ app/src/components/Media.js
@@ -8,6 +8,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 import ErrorReporting from 'material-ui-error-reporting';
 import Nav from './Nav';
 import NewMedia from './NewMedia';
+import MediaGridList from './MediaGridList';
 import axios from 'axios';
 import './Media.css';
 
@@ -17,7 +18,6 @@ const config = require('./../config.json');
 const moment = require('moment');
 const styles = {
   root: {
-    display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     overflowX: 'hidden'
@@ -28,15 +28,11 @@ const styles = {
     overflowY: 'auto',
     padding: 50,
     paddingTop: 10,
+    margin: 0,
     cols: (window.innerWidth > 1000) ? 2 : 1
   },
-  buttonEdit: {
-    top: 10,
-    right: 10,
-    position: 'absolute'
-  },
   buttonDelete: {
-    top: 60,
+    top: 10,
     right: 10,
     position: 'absolute'
   }
@@ -64,47 +60,13 @@ class Media extends Component {
     };
   }
 
-  componentDidMount() {
-    this._getImages();
+  onDone() {
+    this.setState({ showComponent: [true, false] });
   }
 
   _handleTabChange = () => {
     const showComponent = this.state.showComponent.move(0,1);
     this.setState({ showComponent: showComponent });
-  }
-
-  _getImages() {
-    axios.get(this.state.url).then(function(res) {
-      this.setState({ images: res.data });
-    }.bind(this)).catch(err => {
-      this._handleError(err);
-    });
-  }
-
-  _handleEdit(e) {
-    this.props.history.push('/manager/event/edit/' + e.currentTarget.dataset.id);
-  }
-
-  _handleDelete(e) {
-    axios.delete(this.state.url + '/' + e.currentTarget.dataset.id).then(function(res) {
-      this._getImages();
-    }.bind(this)).catch(err => {
-      this._handleError(err);
-    });
-  }
-
-  _handleError(err) {
-    if (!err) {
-      err = new Error('Invalid data');
-    }
-
-    this.setState({
-      error: err
-    });
-
-    setTimeout(function() {
-      this.setState({ error: null });
-    }.bind(this), 5000);
   }
 
   render() {
@@ -113,36 +75,8 @@ class Media extends Component {
         <Nav tabs={this.state.tabs} onTabChange={this._handleTabChange}/>
 
         <div style={styles.root}>
-          { this.state.showComponent[0] ? 
-          <div>
-            <ErrorReporting open={this.state.error !== null}
-              error={this.state.error} />
-
-            <GridList ref="grid" cellHeight={320} cols={styles.gridList.cols} style={styles.gridList}>
-              <Subheader>Media Images</Subheader>
-              {this.state.images.map((event) => (
-                <GridTile key={event.id}
-                          title={event.title}
-                          subtitle={moment(event.date).format("MMM Do YYYY")}
-                          actionIcon={<IconButton><StarBorder color="white" /></IconButton>}>
-                          <img src={config.baseURL + event.preview_img} />
-
-                          <RaisedButton className="edit" 
-                                        label="Edit"
-                                        data-id={event.id}
-                                        onTouchTap={this._handleEdit.bind(this)}
-                                        style={styles.buttonEdit}/>
-                          <RaisedButton className="delete" 
-                                        data-id={event.id}
-                                        onTouchTap={this._handleDelete.bind(this)}
-                                        label="Delete" 
-                                        style={styles.buttonDelete}/>
-                </GridTile>
-              ))}
-            </GridList>
-          </div>  
-          : null }
-          { this.state.showComponent[1] ? <NewMedia onDone="" /> : null }
+          { this.state.showComponent[0] ? <MediaGridList/>: null }
+          { this.state.showComponent[1] ? <NewMedia onDone={this.onDone.bind(this)} /> : null }
         </div>
       </div>
     )
