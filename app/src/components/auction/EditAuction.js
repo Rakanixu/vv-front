d0a86f434e1f0053116625aded64527a138ec46d
Pablo Aguirre <paguirrerubio@gmail.com>
2017-08-21 09:26:06 +0200

    styles edit auctions

--- app/src/components/auction/EditAuction.js
+++ app/src/components/auction/EditAuction.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { dataURItoBlob } from '../../utils';
 import {GridList, GridTile} from 'material-ui/GridList';
+import Paper from 'material-ui/Paper';
 import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -13,8 +14,11 @@ axios.defaults.withCredentials = true;
 
 const config = require('../../config.json');
 var styles = {
-  screenHeight: {
-    height: window.innerHeight - 250
+  paper: {
+    padding: 20,
+    overflow: 'auto',
+    height: 'min-content',
+    width: '100%'
   }
 };
 
@@ -87,29 +91,34 @@ class EditAuction extends Component {
 
   render() {
     return (
-      <div className="container" style={styles.screenHeight}>
+      <div className="container">
         <div className="inner-container">
           <ErrorReporting open={this.state.error !== null}
                     error={this.state.error} />
 
-          <form className="editSliderImage">
-            <TextField floatingLabelText="Name"
-                      data-val="name"
-                      value={this.state.auction.name}
-                      onChange={this._handleTextFieldChange.bind(this)}
-                      fullWidth={true} />
-            <TextField floatingLabelText="Title"
-                      data-val="title"
-                      value={this.state.auction.title}
-                      onChange={this._handleTextFieldChange.bind(this)}
-                      fullWidth={true} />
-            <TextField floatingLabelText="Description"
-                      data-val="description"
-                      value={this.state.auction.description}
-                      onChange={this._handleTextFieldChange.bind(this)}
-                      fullWidth={true} />
+          <form className="edit-acution">
+            <Paper style={styles.paper}>
+              <TextField floatingLabelText="Name"
+                        data-val="name"
+                        value={this.state.auction.name}
+                        onChange={this._handleTextFieldChange.bind(this)}
+                        fullWidth={true} />
+              <TextField floatingLabelText="Title"
+                        data-val="title"
+                        value={this.state.auction.title}
+                        onChange={this._handleTextFieldChange.bind(this)}
+                        fullWidth={true} />
+              <TextField floatingLabelText="Description"
+                        data-val="description"
+                        value={this.state.auction.description}
+                        onChange={this._handleTextFieldChange.bind(this)}
+                        fullWidth={true} />
 
-            <RaisedButton label="Edit" fullWidth={true} onTouchTap={this._handleEditAuction.bind(this)} />
+              <RaisedButton label="Edit" 
+                            className="right margin-top-medium" 
+                            primary={true} 
+                            onTouchTap={this._handleEditAuction.bind(this)} />
+            </Paper>
           </form>
         </div>
       </div>
